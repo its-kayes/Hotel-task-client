719848988ae63b65cc1b4aa2c9fbc27435cbf625
its-kayes <kayes.ek8@gmail.com>
2022-06-11 13:05:49 +0600

    Show get data from mongodb

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from './Loading';
 
 const Hotels = () => {
+
+    let { data: hotels, isLoading, refetch } = useQuery('hotels', () => fetch('http://localhost:5000/hotels').then(res => res.json()));
+
+    // http://localhost:5000/hotels
+    if (isLoading) {
+        return <Loading> </Loading>
+    }
+
     return (
-        <div>
-            <div class="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
-                <div class="card-body">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
+        <div className='w-full flex justify-center'>
+            <div className=' grid grid-cols-3 gap-14 my-20'>
+                {
+                    hotels.map(hotel => <div class="card w-96 bg-base-100 shadow-xl">
+                        <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title"> {hotel.name} </h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>)
+                }
+
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+
+const queryClient = new QueryClient()
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        {/* <Todos /> */}
+
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
