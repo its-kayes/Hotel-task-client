f73362a9928fbd185e10bb4aa5b3d409ca177c6a
its-kayes <kayes.ek8@gmail.com>
2022-06-11 13:16:08 +0600

    Show All hotel on card

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -16,12 +16,14 @@ const Hotels = () => {
             <div className=' grid grid-cols-3 gap-14 my-20'>
                 {
                     hotels.map(hotel => <div class="card w-96 bg-base-100 shadow-xl">
-                        <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
+                        <figure><img src={hotel.img} alt="" /></figure>
                         <div class="card-body">
-                            <h2 class="card-title"> {hotel.name} </h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div class="card-actions justify-end">
-                                <button class="btn btn-primary">Buy Now</button>
+                            <h2 class="card-title text-sky-700 font-bold"> {hotel.name} </h2>
+                            <p className='my-2 font-semibold'> {hotel.location} </p>
+                            <p> {hotel.about} </p>
+                            <div class="card-actions justify-between">
+                                <p className='text-red-600 font-semibold'> Per Day Cost: {hotel.cost} </p>
+                                <button class=" border-2 py-2 px-4 border-green-600 rounded-xl font-bold text-sky-700 "> Details </button>
                             </div>
                         </div>
                     </div>)
